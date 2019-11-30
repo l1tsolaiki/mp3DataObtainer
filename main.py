ad8a98158f698057d04be51625a1e411c109d485
kostya <atrd.konstantin@gmail.com>
2019-11-30 19:07:55 +0300

    Change file name

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import os
 import mutagen
 
-import write_tags
+import writeTags
 from constants import *
 
 
 def process(filename, move=True):
-    write_tags.clearGarbageTags(filename)
-    write_tags.addTags(filename)
+    writeTags.clearGarbageTags(filename)
+    writeTags.addTags(filename)
     if move:
         os.rename(SOURCE + filename, DESTINATION + filename)
 
 
 def main():
-    if input('Warning! You are going to change and possibly move files\n{} ---> {}. \n====Are you sure==== (y/n)? '
-                     .format(SOURCE, DESTINATION)) != 'y':
+    if input('Warning! You are going to change and possibly move files\n'
+             '{} ---> {}.\n'
+             '====Are you sure==== (y/n)? '
+             .format(SOURCE, DESTINATION)) != 'y':
         return 0
 
     for filename in os.listdir(SOURCE):
